a860362401ce2c64fe702833db510f4a0c57f75f
TaniaAkhter78 <chhandabhuiyan78@gmail.com>
2025-05-09 16:00:55 -0400

    Collections examples are done

--- src/com/learning/core/collections/comparatorsandcomparables/MainWithComparator.java
+++ src/com/learning/core/collections/comparatorsandcomparables/MainWithComparator.java
@@ -1,16 +1,20 @@
 package com.learning.core.collections.comparatorsandcomparables;
 
+import java.util.Comparator;
 import java.util.TreeSet;
 
-public class TreeSetExample {
+public class MainWithComparator {
 
     public static void main(String[] args) {
-        // Create a new Student object
+
+        Comparator<Student> studentAgeComparator = Comparator.comparingInt(Student::getAge);
+        Comparator<Student> studentNameComparator = Comparator.comparing(Student::getName);
+
         Student student = new Student("John Doe", 20);
         // Create another Student object
         Student anotherStudent = new Student("Jane Doe", 22);
 
-        TreeSet<Student> students = new TreeSet<>();
+        TreeSet<Student> students = new TreeSet<>(studentNameComparator);
         students.add(student);
         students.add(anotherStudent);
         students.add(new Student("Alice", 22));
@@ -20,6 +24,6 @@ public class TreeSetExample {
 
         System.out.println("TreeSet of Students: " + students);
 
-    }
 
+    }
 }
